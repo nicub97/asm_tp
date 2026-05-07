0c4028b98104ddb903daeffc0e5c1d925df6ec6b
nicubalica <nbalica@myges.fr>
2026-05-07 15:25:53 +0200

    asm07: add atoi to parse stdin input

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -4,12 +4,31 @@ section .bss
 buf resb 16
 
 section .text
+
+atoi:
+    xor rax, rax
+.loop:
+    movzx rcx, byte [rdi]
+    cmp rcx, 10
+    je .done
+    cmp rcx, 0
+    je .done
+    imul rax, rax, 10
+    sub rcx, '0'
+    add rax, rcx
+    inc rdi
+    jmp .loop
+.done:
+    ret
+
 _start:
     mov rax, 0
     mov rdi, 0
     mov rsi, buf
     mov rdx, 16
     syscall
+    mov rdi, buf
+    call atoi
     mov rax, 60
     xor rdi, rdi
     syscall
