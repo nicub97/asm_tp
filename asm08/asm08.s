1563089091be05913e0b67b79eb10a3f54db4164
nicubalica <nbalica@myges.fr>
2026-05-07 16:11:20 +0200

    asm08: init structure and check argv

---
+++ asm08/asm08.s
@@ -0,0 +1,16 @@
+global _start
+
+section .bss
+buf resb 32
+
+section .text
+_start:
+    cmp qword [rsp], 2
+    jl fail
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+fail:
+    mov rax, 60
+    mov rdi, 1
+    syscall
