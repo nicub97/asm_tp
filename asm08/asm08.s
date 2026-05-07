471280d44d9822a5391409c178e8e2c465c8f6c0
nicubalica <nbalica@myges.fr>
2026-05-07 16:11:43 +0200

    asm08: add atoi to parse argv[1]

--- asm08/asm08.s
+++ asm08/asm08.s
@@ -4,9 +4,28 @@ section .bss
 buf resb 32
 
 section .text
+
+atoi:
+    xor rax, rax
+.loop:
+    movzx rcx, byte [rdi]
+    cmp rcx, 0
+    je .done
+    cmp rcx, 10
+    je .done
+    imul rax, rax, 10
+    sub rcx, '0'
+    add rax, rcx
+    inc rdi
+    jmp .loop
+.done:
+    ret
+
 _start:
     cmp qword [rsp], 2
     jl fail
+    mov rdi, [rsp+16]
+    call atoi
     mov rax, 60
     xor rdi, rdi
     syscall
